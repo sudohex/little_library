2f234af130d98aee8f56c4bdcd57d559a98be880
Ellie Tun <n11309270@qut.edu.au>
2023-06-04 11:11:49 +1000

    search working, menu working

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -3,46 +3,45 @@ $(document).ready(function () {
   onDeviceReady();
 });
 
+const baseURL = "http://localhost:3000";
+const endpoint = baseURL + "/api/libraries";
+
+const performAjaxCall = (method, data, successCallback, errorCallback) => {
+  $.ajax({
+    url: endpoint,
+    type: method,
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successCallback,
+    error: errorCallback,
+  });
+};
+
+const fetchAndUpdate = () => {
+  performAjaxCall(
+    "GET",
+    null,
+    function (response) {
+      // Successful API request
+      localStorage.setItem("libraryData", JSON.stringify(response));
+      return response;
+    },
+    function (error) {
+      // Error handling for failed API request
+      console.error("Failed to fetch data: ", error);
+      return [];
+    }
+  );
+};
+
 function onDeviceReady() {
-  const baseURL = "http://localhost:3000";
-  const endpoint = baseURL + "/api/libraries";
-
-  const performAjaxCall = (method, data, successCallback, errorCallback) => {
-    $.ajax({
-      url: endpoint,
-      type: method,
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: successCallback,
-      error: errorCallback,
-    });
-  };
-
-  const fetchAndUpdate = () => {
-    performAjaxCall(
-      "GET",
-      null,
-      function (response) {
-        // Successful API request
-        localStorage.setItem("libraryData", JSON.stringify(response));
-        console.log(response)
-        return response;
-      },
-      function (error) {
-        // Error handling for failed API request
-        console.error("Failed to fetch data: ", error);
-      }
-    );
-  };
-
   // Init data with local data. If data is none fetchAndUpdate().
   let libs = localStorage.getItem("libraryData")
     ? JSON.parse(localStorage.getItem("libraryData"))
     : fetchAndUpdate();
 
   // Navigation Menu
-  $("#dropDownMenu").hide();
   $("#btnMenu")
     .off("click")
     .on("click", function () {
@@ -53,59 +52,67 @@ function onDeviceReady() {
       $("#dropDownMenu").hide();
     }
   });
+  //List of library
+  $("#library-list-page").on("show",function(){
+    displayLibraries(libs)
+  })
 
   //Search and Display
   $("#searchInput").on("input", function () {
     var searchQuery = $(this).val().toLowerCase();
     // Change the libs
-    if (!searchQuery){
-      let libsToDisplay = libs.filter(function (lib) {
-        for (let book of lib.books) {
-          if (
-            book.title.toLowerCase().includes(searchQuery) ||
-            book.author.toLowerCase().includes(searchQuery)
-          ) {
-            return true;
-          }
-        }
-        return false;
-      });
-  
-        displayLibraries(libsToDisplay);
-    }else{
-      displayLibraries(libs)
-    }
 
+    let libsToDisplay = libs.filter(function (lib) {
+      for (let book of lib.books) {
+        if (
+          book.title.toLowerCase().includes(searchQuery) ||
+          book.author.toLowerCase().includes(searchQuery)
+        ) {
+          return true;
+        }
+      }
+      return false;
+    });
+
+    displayLibraries(libsToDisplay);
   });
 
   function displayLibraries(results) {
     $("#search-result").empty();
 
     if (results.length === 0) {
-      $("#search-result").html(
-        '<div><span colspan="3">No results found</span></div>'
-      );
+      $("#search-result").html("<div><span>No results found</span></div>");
     } else {
-      var html = "";
+      var html =
+        '<div data-role="collapsibleset" data-theme="a" data-content-theme="a">';
       for (var i = 0; i < results.length; i++) {
-        html += '<div class="library-header">';
+        html += '<div data-role="collapsible">';
+        html += '<h3 class="library-header">';
+        html += '<img src="library_circle.png" class="library-icon">';
+        html += '<span class="library-name">' + results[i].name + "</span>";
         html +=
-          "<span colspan='3'><sdivong>Library:</sdivong> " +
-          results[i].name +
-          ", " +
-          results[i].address +
-          "</span>";
-        html += "</div>";
+          '<span class="library-address">' + results[i].address + "</span>";
+        html += "</h3>";
 
+        html +=
+          '<ul data-role="listview" data-inset="true" data-theme="a" data-divider-theme="a" class="library-books">';
         for (let book of results[i].books) {
-          html += "<div>";
-          html += "<span>" + book.title + "</span>";
-          html += "<span>" + book.author + "</span>";
-          html += "<span>" + book.description + "</span>";
-          html += "</div>";
+          html += "<li>";
+          html += '<h4 class="book-title">' + book.title + "</h4>";
+          html += '<p class="book-author">' + book.author + "</p>";
+          html += "</li>";
         }
+        html += "</ul>";
+
+        html += "</div>"; // end of collapsible
       }
+      html += "</div>"; // end of collapsibleset
+
       $("#search-result").html(html);
+      $("#search-result [data-role=collapsibleset]")
+        .collapsibleset()
+        .trigger("create");
+      $("#search-result [data-role=listview]").listview().trigger("create");
     }
   }
 }
